bea836354e8d7cafea43b540dc751e0990ce493d
Edsel Caprice <caprice@mail.usf.edu>
2018-11-16 23:12:31 +0000

    automatically backed up by learn

--- db/migrate/01_create_users.rb
+++ db/migrate/01_create_users.rb
@@ -1,8 +1,8 @@
-class CreatUsers < ActiveRecord::Migration
-
-  def change :users do |t|
-    t.string :username
-    t.string :password
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
+    end
   end
-end
 end
