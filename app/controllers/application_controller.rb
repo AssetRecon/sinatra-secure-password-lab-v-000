bd233c779610dbe4780971683d7fa0b7f811b6b0
Edsel Caprice <caprice@mail.usf.edu>
2018-11-16 23:01:45 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < Sinatra::Base
     if params[:username] == ""
       redirect "/failure"
     else
-      User.create(username: params[:username]), password: params[:password])
+      User.create(username: params[:username], password: params[:password])
       redirect "/login"
     end
   end
