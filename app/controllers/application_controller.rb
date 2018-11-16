e4eba3f86b2b29d64560d32f82a4c1c5ad1e2b1a
Edsel Caprice <caprice@mail.usf.edu>
2018-11-16 22:53:18 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,6 +33,9 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     ##your code here
+    if params["username"] = ""
+      redirect to "/failure"
+    end
   end
 
   get "/failure" do
